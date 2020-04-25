8e8b4f91db9019592f9435a37c64f80b214a59ac
Remi <remi@macavity-1.home>
2020-04-25 17:36:15 +0200

    multiProcessing

--- R/Input.r
+++ R/Input.r
@@ -21,31 +21,35 @@ Input = R6::R6Class(
             env$isOtherProcessCheckingThreads = FALSE
             env
         }
-        
+        #shared = {
+           # env <- new.env()
+            #env$optionNames <- c("seed", "random_seed", "printProgress", "nbGens", "nbGenerations", "startAtGeneration", "nbThreads", "S", "species", "nbSubGens", "nbSubGenerations", "PN", "PatchNumber", "N", "patchCapacity", "H", "Habitats", "T5_approximationForNtrl", "T56_approximationForNtrl", "T5_fit", "T5_FitnessEffects", "T5_compressData", "T5_compress", "L", "Loci", "ploidy", "fec", "fecundityForFitnessOfOne", "m", "DispMat", "DispWeightByFitness", "gameteDispersal", "InitialpatchSize", "cloningRate", "selfingRate", "matingSystem", "LogfileType", "sequencingErrorRate", "additiveEffectAmongLoci", "selectionOn", "T1_mu", "T1_MutationRate", "T1_epistasis", "T1_EpistaticFitnessEffects", "T2_mu", "T2_MutationRate", "T4_mu", "T4_MutationRate", "T4_maxAverageNbNodesPerHaplotype", "T5_mu", "T5_MutationRate", "T5_toggleMutsEveryNGeneration", "T5_freqThreshold", "T5_frequencyThresholdForFlippingMeaning", "T3_mu", "T3_MutationRate", "T3_pheno", "T3_PhenotypicEffects", "T3_fit", "T3_FitnessLandscape", "T3_DN", "T3_DevelopmentalNoise", "T1_fit", "T1_FitnessEffects", "T2_fit", "T2_FitnessEffects", "r", "RecombinationRate", "recRateOnMismatch", "FitnessMapInfo", "indTypes", "individualTypes", "resetGenetics", "indIni", "individualInitialization", "T1_ini", "T1_Initial_AlleleFreqs", "T5_ini", "T5_Initial_AlleleFreqs", "GP", "GeneralPath", "T1_vcf_file", "T1_VCF_file", "T1_LargeOutput_file", "T1_AlleleFreq_file", "Log", "Logfile", "Logfile_file", "T1_MeanLD_file", "T1_LongestRun_file", "T1_HybridIndex_file", "T1_ExpectiMinRec_file", "T2_LargeOutput_file", "SaveBinary_file", "T3_LargeOutput_file", "T3_MeanVar_file", "fitness_file", "fitnessSubsetLoci_file", "fitnessStats_file", "T1_FST_file", "T1_FST_info", "extraGeneticInfo_file", "patchSize_file", "extinction_file", "genealogy_file", "coalesce", "shouldGenealogyBeCoalesced", "T4_LargeOutput_file", "T4_vcf_file", "T4_VCF_file", "T4_SFS_file", "T1_SFS_file", "T4_printTree", "T4_coalescenceFst_file", "T5_vcf_file", "T5_VCF_file", "T5_SFS_file", "T5_AlleleFreq_file", "T5_LargeOutput_file", "outputSFSbinSize", "eco", "speciesEcologicalRelationships", "popGrowthModel", "stochasticGrowth", "swapInLifeCycle", "Overwrite", "readPopFromBinary", "DryRun", "centralT1LocusForExtraGeneticInfo", "killOnDemand", "geneticSampling_withWalker", "individualSampling_withWalker")
+            #env
+        #}
     ),
-    active = list(
-        runningThreads = function(value)
-        {
-            if (missing(value))
-            {
-                private$shared$runningThreads
-            } else
-            {
-                private$shared$runningThreads = value
-            }
-        },
+    # active = list(
+    #     runningThreads = function(value)
+    #     {
+    #         if (missing(value))
+    #         {
+    #             private$shared$runningThreads
+    #         } else
+    #         {
+    #             private$shared$runningThreads = value
+    #         }
+    #     },
 
-        isOtherProcessCheckingThreads = function(value)
-        {
-            if (missing(value))
-            {
-                private$shared$isOtherProcessCheckingThreads
-            } else
-            {
-                private$shared$isOtherProcessCheckingThreads = value
-            }
-        }
-    ),
+    #     isOtherProcessCheckingThreads = function(value)
+    #     {
+    #         if (missing(value))
+    #         {
+    #             private$shared$isOtherProcessCheckingThreads
+    #         } else
+    #         {
+    #             private$shared$isOtherProcessCheckingThreads = value
+    #         }
+    #     }
+    # ),
 
 
 
