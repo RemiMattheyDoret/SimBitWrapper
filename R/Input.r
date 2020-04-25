5b0a4e1cf200c8719ad5676a36d6c714ed895f78
Remi <remi@macavity-1.home>
2020-04-25 17:33:45 +0200

    multiProcessing

--- R/Input.r
+++ R/Input.r
@@ -15,28 +15,27 @@ Input = R6::R6Class(
     private = list(
         runningThreads = c(),
         data = "",
-        #shared = {
+        champignon = {
+            env = new.env()
+            env$runningThreads = c()
+            env$isOtherProcessCheckingThreads = FALSE
+            env
+        }
+        #champignon = {
            # env <- new.env()
             #env$optionNames <- c("seed", "random_seed", "printProgress", "nbGens", "nbGenerations", "startAtGeneration", "nbThreads", "S", "species", "nbSubGens", "nbSubGenerations", "PN", "PatchNumber", "N", "patchCapacity", "H", "Habitats", "T5_approximationForNtrl", "T56_approximationForNtrl", "T5_fit", "T5_FitnessEffects", "T5_compressData", "T5_compress", "L", "Loci", "ploidy", "fec", "fecundityForFitnessOfOne", "m", "DispMat", "DispWeightByFitness", "gameteDispersal", "InitialpatchSize", "cloningRate", "selfingRate", "matingSystem", "LogfileType", "sequencingErrorRate", "additiveEffectAmongLoci", "selectionOn", "T1_mu", "T1_MutationRate", "T1_epistasis", "T1_EpistaticFitnessEffects", "T2_mu", "T2_MutationRate", "T4_mu", "T4_MutationRate", "T4_maxAverageNbNodesPerHaplotype", "T5_mu", "T5_MutationRate", "T5_toggleMutsEveryNGeneration", "T5_freqThreshold", "T5_frequencyThresholdForFlippingMeaning", "T3_mu", "T3_MutationRate", "T3_pheno", "T3_PhenotypicEffects", "T3_fit", "T3_FitnessLandscape", "T3_DN", "T3_DevelopmentalNoise", "T1_fit", "T1_FitnessEffects", "T2_fit", "T2_FitnessEffects", "r", "RecombinationRate", "recRateOnMismatch", "FitnessMapInfo", "indTypes", "individualTypes", "resetGenetics", "indIni", "individualInitialization", "T1_ini", "T1_Initial_AlleleFreqs", "T5_ini", "T5_Initial_AlleleFreqs", "GP", "GeneralPath", "T1_vcf_file", "T1_VCF_file", "T1_LargeOutput_file", "T1_AlleleFreq_file", "Log", "Logfile", "Logfile_file", "T1_MeanLD_file", "T1_LongestRun_file", "T1_HybridIndex_file", "T1_ExpectiMinRec_file", "T2_LargeOutput_file", "SaveBinary_file", "T3_LargeOutput_file", "T3_MeanVar_file", "fitness_file", "fitnessSubsetLoci_file", "fitnessStats_file", "T1_FST_file", "T1_FST_info", "extraGeneticInfo_file", "patchSize_file", "extinction_file", "genealogy_file", "coalesce", "shouldGenealogyBeCoalesced", "T4_LargeOutput_file", "T4_vcf_file", "T4_VCF_file", "T4_SFS_file", "T1_SFS_file", "T4_printTree", "T4_coalescenceFst_file", "T5_vcf_file", "T5_VCF_file", "T5_SFS_file", "T5_AlleleFreq_file", "T5_LargeOutput_file", "outputSFSbinSize", "eco", "speciesEcologicalRelationships", "popGrowthModel", "stochasticGrowth", "swapInLifeCycle", "Overwrite", "readPopFromBinary", "DryRun", "centralT1LocusForExtraGeneticInfo", "killOnDemand", "geneticSampling_withWalker", "individualSampling_withWalker")
             #env
         #}
-
-        shared = {
-            env <- new.env()
-            env$runningThreads <- c()
-            env$isOtherProcessCheckingThreads <- FALSE
-            env
-        }
     ),
     active = list(
         runningThreads = function(value)
         {
             if (missing(value))
             {
-                private$shared$runningThreads
+                private$champignon$runningThreads
             } else
             {
-                private$shared$runningThreads <- value
+                private$champignon$runningThreads = value
             }
         },
 
@@ -44,10 +43,10 @@ Input = R6::R6Class(
         {
             if (missing(value))
             {
-                private$shared$isOtherProcessCheckingThreads
+                private$champignon$isOtherProcessCheckingThreads
             } else
             {
-                private$shared$isOtherProcessCheckingThreads <- value
+                private$champignon$isOtherProcessCheckingThreads = value
             }
         }
     ),
@@ -94,9 +93,9 @@ Input = R6::R6Class(
             stopifnot(class(optionName) == "character")
             stopifnot(length(optionName) == 1)
 
-            # if (!any(optionName == private$shared$optionNames))
+            # if (!any(optionName == private$champignon$optionNames))
             # {
-            #     distances = stringdist(optionName, private$shared$optionNames)
+            #     distances = stringdist(optionName, private$champignon$optionNames)
             #     alternatives = optionNames[which(order(distances) < 3)]
 
             #     stop(
@@ -138,11 +137,11 @@ Input = R6::R6Class(
         ### print options
         #catAllOptions = function()
         #{
-        #    print(private$shared$optionNames)
+        #    print(private$champignon$optionNames)
         #},
 
         ### Print current command
-        catCommand = function(oneLine)
+        catCommand = function(oneLine=FALSE)
         {
             if (oneLine)
             {
@@ -159,23 +158,23 @@ Input = R6::R6Class(
             {
                 Sys.sleep(0.001)
             }
-            private$shared$isOtherProcessCheckingThreads = TRUE
+            private$champignon$isOtherProcessCheckingThreads = TRUE
 
             thread_index = 1
-            while (thread_index <= length(private$shared$runningThreads))
+            while (thread_index <= length(private$champignon$runningThreads))
             {
-                thread = private$shared$runningThreads[thread_index]
+                thread = private$champignon$runningThreads[thread_index]
                 if (!thread$is_alive())
                 {
-                    private$shared$runningThreads = private$shared$runningThreads[-thread_index]
+                    private$champignon$runningThreads = private$champignon$runningThreads[-thread_index]
                 } else
                 {
                     thread_index = thread_index + 1
                 }
             }
 
-            r = length(private$shared$runningThreads) <  maxNbThreads
-            private$shared$isOtherProcessCheckingThreads = FALSE
+            r = length(private$champignon$runningThreads) <  maxNbThreads
+            private$champignon$isOtherProcessCheckingThreads = FALSE
             return(r)
         },
 
@@ -194,7 +193,7 @@ Input = R6::R6Class(
 
             #newThread = system(paste(executable, paste(parameters, collapse=" "), "& &> /dev/null; echo $!"), intern = TRUE)
 
-            private$shared$runningThreads = c(private$shared$runningThreads, newThread)
+            private$champignon$runningThreads = c(private$champignon$runningThreads, newThread)
         
         }
     )
