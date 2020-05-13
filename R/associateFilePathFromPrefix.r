610558c43ab0dcc9eac73705effe6c7fdb6b9b1f
Remi <remi@macavity-1.home>
2020-05-13 09:55:25 +0200

    associate file from prefix

---
+++ R/associateFilePathFromPrefix.r
@@ -0,0 +1,16 @@
+associateFilePathFromPrefix(prefix)
+{
+	directory = sub("/[^/]+$","",prefix)
+	pattern   = paste0(sub(".*/.*[^/]/+","",prefix), "\\.")
+	path = paste0(directory, "/",list.files(path=directory, pattern=pattern))
+	if (length(path) == 0)
+	{
+		stop(paste0("In 'getFilePathFromPrefix', (maybe called from the default function 'fun' of 'gatherData'), tried to find a single file associate with prefix '", prefix, "'. (dir:", directory, " | pattern:", pattern, ") but none was found."))
+	}
+	if (length(path) > 1)
+	{
+		path = path[!grepl("*\\.*[^.]", path)]
+		stop(paste0("In 'getFilePathFromPrefix', (maybe called from the default function 'fun' of 'gatherData'), tried to find a single file associate with prefix '", prefix, "'. (dir:", directory, " | pattern:", pattern, ") but it found ", length(path), " files."))
+	}
+	return(path)
+}
--- R/defaultFunctionForGatherData.r
+++ R/defaultFunctionForGatherData.r
@@ -1,17 +1,6 @@
-defaultFunctionForGatherData = function(path)
+defaultFunctionForGatherData = function(prefix)
 {
-	directory = sub("/[^/]+$","",path)
-	pattern   = paste0(sub(".*/.*[^/]/+","",path), "\\.")
-	file = paste0(directory, "/",list.files(path=directory, pattern=pattern))
-	if (length(file) == 0)
-	{
-		stop(paste0("In the default function 'fun' of 'gatherData', tried to find a single file associate with path '", path, "'. (dir:", directory, " | pattern:", pattern, ") but none was found."))
-	}
-	if (length(file) > 1)
-	{
-		file = file[!grepl("*\\.*[^.]", file)]
-		stop(paste0("In the default function 'fun' of 'gatherData', tried to find a single file associate with path '", path, "'. (dir:", directory, " | pattern:", pattern, ") but it found ", length(file), " files."))
-	}
+	path = getFilePathFromPrefix(prefix)
 
 	d = data.table::fread(file, header=TRUE)
 	if (nrow(d) > 1)
