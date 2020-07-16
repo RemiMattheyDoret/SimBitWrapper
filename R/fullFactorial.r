c77bc13d9174e356f3534863d247786569882406
Remi <remi@Macavity.local>
2020-07-16 14:03:37 +0200

    Back to old FullFactorial function

--- R/fullFactorial.r
+++ R/fullFactorial.r
@@ -2,7 +2,53 @@ fullFactorial = function(..., outputFilePrefix = NULL)
 {
 	input = list(...)
 	
-	SGrid = expand.grid(input)
+	Names = rep("",length(input))
+	for (big in 1:length(input))
+	{
+		if (class(input[[big]]) == "list")
+		{
+			name = names(input[[big]])[1]
+		} else
+		{
+			name = names(input)[big]
+		}
+			
+		if (length(name)<1) stop("error: Name missing")
+		if (length(name)>1) stop("error: length(name)>1")
+		Names[big] = name
+	}
+	
+
+	FirstCol = list()
+	for (elem in input)
+	{
+		if (class(elem)=="list")
+		{
+			FirstCol[[length(FirstCol)+1]] = elem[[1]]
+		} else
+		{
+			FirstCol[[length(FirstCol)+1]] = elem
+		}
+	}
+
+	SGrid = expand.grid(FirstCol)
+	Cols = FirstCol
+	for (big in 1:length(input))
+	{
+		if (class(input[[big]]) == "list")
+		{
+			for (small in 2:length(input[[big]]))
+			{
+				Cols[[big]] = input[[big]][[small]]
+				name = names(input[[big]][small])
+				if (length(name)<1) stop("error: Name missing")
+				if (length(name)>1) stop("error: length(name)>1")
+				Names[length(Names)+1] = name
+				SGrid = cbind(SGrid,expand.grid(Cols)[,big])
+			}
+		}
+	}
+	names(SGrid) = Names
 
 	if (!is.null(outputFilePrefix))
 	{
@@ -13,59 +59,15 @@ fullFactorial = function(..., outputFilePrefix = NULL)
 }
 
 
-# Old more complex full factoria:
+
+# Old much simpler full factorial
 
 # fullFactorial = function(..., outputFilePrefix = NULL)
 # {
 # 	input = list(...)
+# 	print(input)
 	
-# 	Names = rep("",length(input))
-# 	for (big in 1:length(input))
-# 	{
-# 		if (class(input[[big]]) == "list")
-# 		{
-# 			name = names(input[[big]])[1]
-# 		} else
-# 		{
-# 			name = names(input)[big]
-# 		}
-			
-# 		if (length(name)<1) stop("error: Name missing")
-# 		if (length(name)>1) stop("error: length(name)>1")
-# 		Names[big] = name
-# 	}
-	
-
-# 	FirstCol = list()
-# 	for (elem in input)
-# 	{
-# 		if (class(elem)=="list")
-# 		{
-# 			FirstCol[[length(FirstCol)+1]] = elem[[1]]
-# 		} else
-# 		{
-# 			FirstCol[[length(FirstCol)+1]] = elem
-# 		}
-# 	}
-
-# 	SGrid = expand.grid(FirstCol)
-# 	Cols = FirstCol
-# 	for (big in 1:length(input))
-# 	{
-# 		if (class(input[[big]]) == "list")
-# 		{
-# 			for (small in 2:length(input[[big]]))
-# 			{
-# 				Cols[[big]] = input[[big]][[small]]
-# 				name = names(input[[big]][small])
-# 				if (length(name)<1) stop("error: Name missing")
-# 				if (length(name)>1) stop("error: length(name)>1")
-# 				Names[length(Names)+1] = name
-# 				SGrid = cbind(SGrid,expand.grid(Cols)[,big])
-# 			}
-# 		}
-# 	}
-# 	names(SGrid) = Names
+# 	SGrid = expand.grid(input)
 
 # 	if (!is.null(outputFilePrefix))
 # 	{
