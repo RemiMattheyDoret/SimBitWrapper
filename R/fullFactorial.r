0a17ed416dab358f9ad21f751e42cd325677dee0
Remi <remi@macavity.home>
2020-09-02 12:52:31 +0200

    fulFactorial accepts nested lists

--- R/fullFactorial.r
+++ R/fullFactorial.r
@@ -1,6 +1,7 @@
 fullFactorial = function(..., simIDprefix = NULL)
 {
 	input = list(...)
+	#print(input)
 	
 	Names = rep("",length(input))
 	for (big in 1:length(input))
@@ -13,10 +14,11 @@ fullFactorial = function(..., simIDprefix = NULL)
 			name = names(input)[big]
 		}
 			
-		if (length(name)<1) stop("error: Name missing")
-		if (length(name)>1) stop("error: length(name)>1")
+		#if (length(name)<1) stop("error: Name missing")
+		#if (length(name)>1) stop("error: length(name)>1")
 		Names[big] = name
 	}
+	#print(Names)
 	
 
 	FirstCol = list()
@@ -35,16 +37,24 @@ fullFactorial = function(..., simIDprefix = NULL)
 	Cols = FirstCol
 	for (big in 1:length(input))
 	{
-		if (class(input[[big]]) == "list")
+		if (length(input[[big]]) > 1)
 		{
 			for (small in 2:length(input[[big]]))
 			{
+				#print(paste0("big = ", big))
+				#print(paste0("small = ", small))
 				Cols[[big]] = input[[big]][[small]]
 				name = names(input[[big]][small])
 				if (length(name)<1) stop("error: Name missing")
 				if (length(name)>1) stop("error: length(name)>1")
+				if (any(Names == name)) stop(paste0("Name '", name, "' is present more than once"))
 				Names[length(Names)+1] = name
-				SGrid = cbind(SGrid,expand.grid(Cols)[,big])
+				#print("Current cols:")
+				#print(Cols)
+				#print("adding column:")
+				#print(expand.grid(Cols)[,big])
+				SGrid[[name]] = expand.grid(Cols)[,big]
+				#SGrid = cbind(SGrid,expand.grid(Cols)[,big]) This does not work if using list of lists
 			}
 		}
 	}
