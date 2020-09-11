c9e5c3506baec3a6a3e9eb867566f522e54da2fd
Remi <remi@Macavity.home>
2020-09-11 16:19:54 +0200

    fullFactorial allowing sublists

--- R/defaultFunctionForGatherData.r
+++ R/defaultFunctionForGatherData.r
@@ -2,7 +2,7 @@ defaultFunctionForGatherData = function(prefix)
 {
 	path = associateFilePathFromPrefix(prefix)
 
-	d = data.table::fread(file, header=TRUE)
+	d = data.table::fread(path, header=TRUE)
 	if (nrow(d) > 1)
 	{
 		d = d[-(1:(nrow(d)-1)),]
