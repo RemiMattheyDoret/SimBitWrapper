f18bc530d106a7bc1030a51691e1f9bcfdad9758
Remi <remi.b.md@gmail.com>
2021-01-20 15:54:53 +0100

    test_wrapperCurrentlyUnavailable

--- SimBitWrapper/R/runSimBit.r
+++
@@ -1,9 +0,0 @@
-### runSimulation
-runSimBit = function(executable, path, lineNumber)
-{
-    stopifnot(class(executable) == "character")
-    stopifnot(length(executable) == 1)
-    stopifnot(class(path) == "character")
-    stopifnot(length(path) == 1)
-    system(paste(executable, "f", path, lineNumber))
-}
