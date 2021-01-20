9417a2293807c132c486370d4e15c28e4737d269
Remi <remi.b.md@gmail.com>
2021-01-20 21:20:18 +0100

    test_wrapperCurrentlyUnavailable

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 reduceString <- function(x) {
-    .Call(`_SimBitWrapper_reduceString`,x)
+    .Call(`_SimBitWrapper_reduceString`, x)
 }
 
